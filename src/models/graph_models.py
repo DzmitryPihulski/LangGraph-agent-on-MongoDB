b29549743c661153df1bf556efd6403f67fbb26d
Dzmitry Pihulski <dim.pigulsky@gmail.com>
2024-10-22 14:56:45 +0200

    requirements updated; bug fixed

--- src/models/graph_models.py
+++ src/models/graph_models.py
@@ -1,4 +1,4 @@
-from typing import Annotated, List
+from typing import Annotated, List, Union
 
 from langchain_core.messages import AIMessage, HumanMessage
 from langgraph.graph.message import add_messages  # type: ignore
@@ -8,7 +8,7 @@ from typing_extensions import TypedDict
 class State(TypedDict):
     human_input: Annotated[str, "The initial query."]
     messages: Annotated[
-        List[AIMessage | HumanMessage],
+        List[Union[AIMessage, HumanMessage, None]],
         add_messages,
         "Messages between nodes.",
     ]
